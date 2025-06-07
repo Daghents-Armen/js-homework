d963573c4f9670237290888c8207467746ba79a9
Daghents-Armen <armen.daghents@gmail.com>
2025-06-07 13:47:21 +0400

    added homework for 20250527

---
+++ 20250527/homework.js
@@ -0,0 +1,109 @@
+//let str = "hello"
+
+//function strcount(str) {
+  //  let count = 0;
+   // for (let i = 0; i < str.length; ++i) {
+     //   ++count
+   // }
+   // return count;
+//}
+
+//let res = strcount(str);
+//console.log(res);
+
+//let str = "anaconda";
+
+//function converttoupper(str) {
+  //  let res = '';
+
+   // for (let i = 0; i < str.length; ++i) {
+     //   if (str[i] >= 'a' && str[i] <= 'z') {
+      //      res += str[i].toUpperCase();
+       // } else {
+         //   res += str[i]
+       // }
+   // }
+
+   // return res;
+//}
+
+//let result = converttoupper(str);
+//console.log(result);
+
+
+//let x = 5;
+//let y = 5;
+
+//function sumofnum(x, y) {
+  //  return x + y;
+//}
+
+//let res = sumofnum(x,y);
+//console.log(res);
+
+//let str = "hello";
+//let res = '';
+
+//for (let i = str.length - 1; i >= 0; --i) {
+  // res += str[i];
+//}
+
+//console.log(res);
+
+//let arr1 = [1,2];
+//let arr2 = [3,4];
+//let arr3 = [];
+//let k = 0;
+
+   // for (let i = 0; i < arr1.length; ++i) {
+     //   arr3[k++] = arr1[i];
+   // }
+
+   // for (let i = 0; i < arr2.length; ++i) {
+     //   arr3[k++] = arr2[i];
+   // }
+
+    //console.log(arr3);
+
+//let str1 = "Learning JavaScript";
+//let str2 = "Java";
+//let k = 0;
+//let res = '';
+//let i = 0;
+
+//while (i < str1.length) {
+  //  if (str1[i] == str2[k]) {
+   // res += str2[k++]
+    //++i;
+   // } else {
+    //    ++i;
+   // }
+//}
+
+//console.log(res);
+
+//let arr1 = [3,6,9,12];
+//let size = arr1.length;
+
+//for (let i = 0; i < size; ++i) {
+  //  if (arr1[i] == 9) {
+    //    console.log(i);
+    //    return;
+   // }
+//}
+
+//let arr = [1,2,3,4,5,6];
+//let size = arr.length;
+//let sum = 0;
+
+//function sumofnums(arr, size) {
+//for (let i = 0; i < size; ++i) {
+  //  sum += arr[i];
+//}
+
+  //  return sum;
+//}
+
+//let res = sumofnums(arr,size);
+//console.log(res);
+
